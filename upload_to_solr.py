2a6adfbb373f8def1725f92182d05d497a7a373d
Baymax15 <39257699+Baymax15@users.noreply.github.com>
2021-11-16 20:21:39 +0530

    posting to solr
    configure schema
    add data according to schema

--- upload_to_solr.py
+++ upload_to_solr.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 import requests
 from pg_python import pg_python as pg_py
@@ -8,13 +9,80 @@ db_name = 'postgres'
 username = 'postgres'
 password = 'postgres'
 host_address = 'localhost'
-table_name = 'ndtv_india2'
+table_name = 'ndtv_india'
 pgs = pg_py.pg_server(db_name, username, password, host_address)
 
-data = pg_py.read(table_name, ['title', 'link', 'date', 'author', 'place', 'description', 'content'], {'1': '1'})
-for record in data:
-    rec_json = json.dumps(record)
-    if not rec_json:
+fields = {
+    'id': 'string',
+    'title': 'text_general',
+    'link': 'text_general',
+    'date': 'pdates',
+    'author': 'text_general',
+    'place': 'text_general',
+    'description': 'text_general',
+    'content': 'text_general'
+}
+
+core = 'ndtv_india'
+base_url = "http://localhost:8983/solr"
+url = '{}/{}'.format(base_url, core)
+headers = {'Content-Type': 'application/json'}
+
+print('Setting schema...')
+# setup no auto create fields
+no_auto_create = {
+    'set-user-property': {
+        'update.autoCreateFields': False
+    }
+}
+requests.post('{}/config'.format(url), headers=headers, data=json.dumps(no_auto_create))
+
+# setup fields
+for field in fields:
+    if field == 'id':
         continue
-    requests.post("http://localhost:8983/solr/ndtv_india/update?wt=json", headers={"Content-Type": "application/json"},
-                  data='{"add":{ "doc":' + rec_json + ',"overwrite":true, "commitWithin": 1000}}')
+    payload = {
+        'add-field': {
+            'name': field,
+            'type': fields[field],
+            'multiValued': False,
+            'stored': True
+        }
+    }
+    resp = json.loads(requests.post('{}/schema'.format(url), headers=headers, data=json.dumps(payload)).text)
+    status = resp.get('responseHeader').get('status')
+    if status == 400:
+        print(resp.get('error').get('details')[0].get('errorMessages')[0].strip())
+    elif status == 0:
+        print(field, 'ok')
+        copy = {
+            'add-copy-field': {
+                'source': field,
+                'dest': '_text_'
+            }
+        }
+        requests.post('{}/schema'.format(url), headers=headers, data=json.dumps(copy))
+    else:
+        print('Error:\n{}'.format(resp))
+
+print('Schema configured.')
+
+print('importing data...')
+data = pg_py.read(table_name, fields.keys(), {'1': '1'})
+
+for record in data:
+    rec = record.copy()
+    date = datetime.strptime(record['date'], '%Y-%m-%d')
+    rec.update({'date': date.isoformat() + 'Z'})
+    rec_json = json.dumps({
+        'add': {
+            'doc': rec,
+            'overwrite': True,
+            'commitWithin': 1000
+        }
+    })
+    resp = json.loads(requests.post("{}/update?wt=json".format(url), headers=headers, data=rec_json).text)
+    if resp.get('responseHeader').get('status') != 0:
+        print(resp.get('error').get('msg').strip())
+
+print('imported data.\n{}/#/{}/query'.format(base_url, core))
